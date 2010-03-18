50583bcc6f5fd97f5fcba263ca96b6b056a41b21
Karl Bennett <pyr0@eve.(none)>
2010-03-18 23:53:53 +0000

    Added the constants and globals files I forgot in the last commit.

---
+++ globals.lisp
@@ -0,0 +1,6 @@
+; Global Variables
+(defparameter *not-found-page-uri* nil)
+(defparameter *not-found-default-page* "Page Not Found")
+
+(defparameter *bad-request-page-uri* nil)
+(defparameter *bad-request-default-page* "Bad Request")
